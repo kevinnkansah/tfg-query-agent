d556bc196ff7ba5663a2a9dc786928961c2aef86
kevinnkansah <kevinnkansah6@gmail.com>
2025-06-30 18:22:16 +0200

    increase timeout for MCP server

--- query-agent/agent.py
+++ query-agent/agent.py
@@ -1,24 +1,27 @@
 from google.adk.agents import Agent, LoopAgent, SequentialAgent
 from google.adk.models.lite_llm import LiteLlm
-from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters, StdioConnectionParams
 from google.adk.tools.tool_context import ToolContext
 
 query_tool = MCPToolset(
-        connection_params=StdioServerParameters(
-            command="npx",
-            args=[
-                "-y",
-                "@benborla29/mcp-server-mysql"
-            ],
-            env={
-                "MYSQL_PORT": "3306",
-                "MYSQL_USER": "root",
-                "ALLOW_INSERT_OPERATION": "false",
-                "ALLOW_UPDATE_OPERATION": "false",
-                "ALLOW_DELETE_OPERATION": "false",
-            }
+        connection_params=StdioConnectionParams(
+            timeout=60,
+            server_params=StdioServerParameters(
+                command="npx",
+                args=[
+                    "-y",
+                    "@benborla29/mcp-server-mysql"
+                ],
+                env={
+                    "MYSQL_PORT": "3306",
+                    "MYSQL_USER": "root",
+                    "ALLOW_INSERT_OPERATION": "false",
+                    "ALLOW_UPDATE_OPERATION": "false",
+                    "ALLOW_DELETE_OPERATION": "false",
+                }
+            )
         )
-    )
+)
 
 def exit_loop(tool_context: ToolContext):
     """
